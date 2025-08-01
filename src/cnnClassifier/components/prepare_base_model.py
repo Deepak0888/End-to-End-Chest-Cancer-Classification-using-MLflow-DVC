8ec39b55fa493f20a2829423f7324c3a84bb7287
Deepak0888 <deepaku0222@gmail.com>
2025-08-01 16:25:45 +0530

    model trainer added

--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -4,6 +4,10 @@ from zipfile import ZipFile
 import tensorflow as tf
 from pathlib import Path
 from cnnClassifier.entity.config_entity import PrepareBaseModelConfig
+                                                
+
+
+
 
 class PrepareBaseModel:
     def __init__(self, config: PrepareBaseModelConfig):
--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -2,3 +2,6 @@ from pathlib import Path
 
 CONFIG_FILE_PATH = Path("config/config.yaml")
 PARAMS_FILE_PATH = Path("params.yaml")
+
+print("CONFIG_FILE_PATH exists:", CONFIG_FILE_PATH.exists())
+print("PARAMS_FILE_PATH exists:", PARAMS_FILE_PATH.exists())
